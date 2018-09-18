9e04d67028980d2a6f8280fd2ff891120bcdffc8
Kenny <kenny@yeoyou.net>
2018-09-18 22:51:44 +1000

    stop install system tools

--- init.sh
+++ init.sh
@@ -2,19 +2,19 @@
 
 pushd ~
 
-case $(uname -s) in
-Darwin)
-  # -- Homebrew
-  ~/dotfiles/brew.sh
-  # -- Karabiner
-  ln -s dotfiles/karabiner ~/.config
-  # To maximize MacVim's window horizontally as well as vertically
-  defaults write org.vim.MacVim MMZoomBoth 1
-  ;;
-Linux)
-  ~/dotfiles/ubuntu.sh
-  ;;
-esac
+# case $(uname -s) in
+# Darwin)
+#   # -- Homebrew
+#   ~/dotfiles/brew.sh
+#   # -- Karabiner
+#   ln -s dotfiles/karabiner ~/.config
+#   # To maximize MacVim's window horizontally as well as vertically
+#   defaults write org.vim.MacVim MMZoomBoth 1
+#   ;;
+# Linux)
+#   ~/dotfiles/ubuntu.sh
+#   ;;
+# esac
 
 # -- Vim
 ln -s dotfiles/.vim .vim
@@ -22,16 +22,16 @@ ln -s dotfiles/.vimrc .vimrc
 ln -s dotfiles/.gvimrc .gvimrc
 ln -s dotfiles/.ideavimrc .ideavimrc
 
-# -- NeoVim
-mkdir -p ~/.config
-pushd ~/.config
-ln -s ~/dotfiles/.vim nvim
-popd
-# For python3 support. Otherwise you'll get:
-# [deoplete] deoplete.nvim does not work with this version.
-# [deoplete] It requires Neovim with Python3 support("+python3").
-pip3 install --upgrade pip
-pip3 install neovim
+# # -- NeoVim
+# mkdir -p ~/.config
+# pushd ~/.config
+# ln -s ~/dotfiles/.vim nvim
+# popd
+# # For python3 support. Otherwise you'll get:
+# # [deoplete] deoplete.nvim does not work with this version.
+# # [deoplete] It requires Neovim with Python3 support("+python3").
+# pip3 install --upgrade pip
+# pip3 install neovim
 
 # dein.vim
 pushd dotfiles
@@ -44,10 +44,3 @@ ln -s dotfiles/.tmux.conf .tmux.conf
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# -- Hyper
-ln -s dotfiles/.hyper.js .hyper.js
-
-# -- Alacritty
-ln -s dotfiles/.alacritty.yml .alacritty.yml
-
-popd
