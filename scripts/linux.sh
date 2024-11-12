197327c8ad0cd7eb79f430522357ae73952c5c38
Kenny Yeo <kenny@yeoyou.net>
2024-11-12 23:15:05 +1100

    feat: add terraform

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -18,6 +18,18 @@ fi
 
 role=${1:base}
 
+if [ "$role" != "base" ]; then
+  echo "============================
+Role: $role
+============================="
+  if [ "$role" = "linuxdev" ]; then
+    echo "Will also install...
+  - terraform
+
+"
+  fi
+fi
+
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
 distro_name=$(lsb_release -i|cut -f2)
 
@@ -71,7 +83,7 @@ pipx install pynvim
 if [ "$role" = "linuxdev" ]; then
 
   # terraform
-  wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+  wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg -f --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
   sudo apt update && sudo apt install terraform
 
