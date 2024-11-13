303818f4a8595a4ac1ce5bc596692132c798edc6
Kenny Yeo <kenny@yeoyou.net>
2024-11-14 00:38:36 +1100

    fix: add ansible

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -81,12 +81,13 @@ if [ "$skip_devtools" ]; then exit 0; fi
 pipx install pynvim
 
 if [ "$role" = "linuxdev" ]; then
-
   # terraform
   wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg -f --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
   sudo apt update && sudo apt install terraform
 
+  # ansible
+  pipx install --include-deps ansible
 fi
 
 # homebrew
