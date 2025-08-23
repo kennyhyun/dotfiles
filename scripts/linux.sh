295736b8f1b6bad157ac6a34bc9512f0919d63cd
Kenny Yeo <kenny@yeoyou.net>
2025-08-23 17:05:29 +1000

    fix(linux): install script

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,3 +1,16 @@
+# linux.sh
+#
+# system utils for Linux
+#
+# sudo required
+#
+# for ubuntu/debian
+# x64, arm64
+#
+
+
+set -e
+
 #!/bin/bash
 if [ "$PRODUCTION" ]; then
   skip_devtools=1
@@ -5,12 +18,14 @@ fi
 
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
 distro_name=$(lsb_release -i|cut -f2)
+
 if [ "$distro_name" = "Ubuntu" ]; then
   perf_package=linux-tools-common
 else
   perf_package=linux-perf
 fi
 
+# Stop if sudo is unavailable
 if [ -z "$(sudo ls)" ]; then
   echo "Please install sudo package"
   echo "eg.
@@ -24,28 +39,33 @@ And please login again
 fi
 
 sudo apt update
+
+# Install essential tools
 sudo apt install -y \
   $perf_package \
   apt-transport-https \
   ca-certificates \
   software-properties-common \
   git \
+  htop \
   python3-pip \
+  python3-venv \
+  pipx \
   jq \
   curl \
   default-jre \
   tmux \
+  silversearcher-ag \
+  zsh \
   net-tools
 
 if [ "$skip_devtools" ]; then exit 0; fi
 
+# Install dev tools
+
 sudo apt install -y \
-  htop \
   vim-gtk3 `# with python-vim` \
-  silversearcher-ag \
-  zsh \
   graphviz \
-  pipx \
   python3-pynvim `# vim plugin` \
 
 # delta for gitdiff
@@ -61,24 +81,4 @@ fi
 
 # pip
 pipx install httpie
-
-## docker-compose
-#if [ -f "/usr/local/bin/docker-compose" ]; then
-#  echo "-----\ndocker-compose aleady exists"
-#  docker-compose --version
-#else
-#  echo "-----\nInstalling docker-compose...."
-#  pipx install requests
-#  dc_version=${COMPOSE_VERSION:-1.29.2}
-#  dc_version_url=/docker/compose/releases/download/${dc_version}/docker-compose-$(uname -s)-$(uname -m)
-#  if [ -z "$dc_version_url" ];then
-#    echo "Could not find the docker-compose url, please install manually from $github_compose_release_url"
-#  else
-#    docker_compose_url=https://github.com${dc_version_url}
-#    echo Downloading: $docker_compose_url
-#    sudo wget $docker_compose_url -O /usr/local/bin/docker-compose -q --show-progress --progress=bar:force
-#    sudo chmod +x /usr/local/bin/docker-compose
-#    docker-compose --version
-#  fi
-#fi
 
